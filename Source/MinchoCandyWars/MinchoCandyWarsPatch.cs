b03f140702f53262a75bccaef187148e8ea62839
在戴森球上打螺丝 <kearril0528@outlook.com>
2026-01-27 18:56:24 +0800

    简单初始化

---
+++ Source/MinchoCandyWars/MinchoCandyWarsPatch.cs
@@ -0,0 +1,15 @@
+﻿using HarmonyLib;
+using Verse;
+
+namespace MinchoCandyWars
+{
+    [StaticConstructorOnStartup]
+    public static class MinchoCandyWarsPatch
+    {
+        static MinchoCandyWarsPatch()
+        {
+            var harmony = new Harmony("MinchoCandyWarsPatch");
+            harmony.PatchAll();
+        }
+    }
+}
